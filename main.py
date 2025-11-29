6a0e1547e2868a5ce19cb4ee2fa129029cdf7c08
IAMYOU-YOUYESYOU <arnavtomar99@gmail.com>
2025-11-29 17:50:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import os
 import requests
 import json
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # -----------------------------
 # GROQ API CONFIG
