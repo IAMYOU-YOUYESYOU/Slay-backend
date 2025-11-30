f5a79aca091c551dd30b1d017d288b7212682d56
IAMYOU-YOUYESYOU <arnavtomar99@gmail.com>
2025-11-30 13:42:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -102,4 +102,5 @@ def chat_api():
 # RUN FLASK SERVER
 # -----------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
